ea91dfe228a1f3945344af9d48a8ca082cdf4383
godmodevegeta <kshubham1904@gmail.com>
2025-02-08 16:23:10 +0530

    html templates

--- flask-app.py
+++ flask-app.py
@@ -1,14 +1,21 @@
-from flask import Flask, request
+from flask import Flask, request, make_response
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if (request.method == "GET"):
+        return "You made a GET request\n", 200
+    elif (request.method == "POST"):
+        return "You made a POST request\n", 201   
+    return "<h1>Hello, World!</h1>\n"
+
+@app.route("/hello/")
 def hello():   
-    return "<h1>Hello, World!</h1>"
-
-@app.route("/greet/<name>")
-def greet(name):   
-    return f"<h1>Hello, {name}!</h1>"
+    response = make_response("Hello World!\n")
+    response.status_code = 200
+    response.content_type = "text/plain"
+    return response
 
 @app.route("/login")
 def login():
