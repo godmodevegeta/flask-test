6401b572198ae8af70f470747ca56a5adfc3fd9b
godmodevegeta <kshubham1904@gmail.com>
2025-02-21 11:38:48 +0530

    Initialize Supabase client and fetch planet names from the database

--- supa.py
+++ supa.py
@@ -2,14 +2,15 @@ import os
 from dotenv import load_dotenv
 from supabase import create_client, Client
 
-# load_dotenv()
+load_dotenv()
 
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
-test: str = "just checking"
-# supabase: Client = create_client(url, key)
+supabase: Client = create_client(url, key)
 
-print("SUPABASE-URL: ", url)
-print("SUPABASE-KEY: ", key)
-print("TEST: ", test)
+response = supabase.table("planets").select("*").execute()
+
+for i in response.data:
+    print(i.get('name'))
+# print(response)
 
